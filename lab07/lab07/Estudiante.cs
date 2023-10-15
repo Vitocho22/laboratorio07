6aa4825dd959ca051454a26e8d77abf487fb92ed
VSinche <115099082+Vitocho22@users.noreply.github.com>
2023-10-14 22:32:40 -0500

    Agregar archivos de proyecto.

---
+++ lab07/lab07/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab07
+{
+    public class Estudiante
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Seccion { get; set; }
+    }
+}
---
+++ lab07/lab07/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<Estudiante> estudiantes1 = new List<Estudiante>
+            {
+                new Estudiante { Nombres = "Alejandro", Apellidos = "Gutiérrez", Edad = 20, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "Lorena", Apellidos = "Sánchez", Edad = 19, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "Ricardo", Apellidos = "López", Edad = 22, Seccion = "Seccion A" },
+                new Estudiante { Nombres = "Isabel", Apellidos = "Torres", Edad = 21, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Gustavo", Apellidos = "Mendoza", Edad = 18, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Paula", Apellidos = "Pérez", Edad = 23, Seccion = "Seccion B" },
+                new Estudiante { Nombres = "Roberto", Apellidos = "Fernández", Edad = 20, Seccion = "Seccion C" },
+                new Estudiante { Nombres = "Natalia", Apellidos = "Gómez", Edad = 22, Seccion = "Seccion C" },
+                new Estudiante { Nombres = "Luisa", Apellidos = "Hernández", Edad = 24, Seccion = "Seccion C" },
+            };
+
+            var groupedStudents = estudiantes1.GroupBy(e => e.Seccion);
+
+            lvEstudiantes1.ItemsSource = groupedStudents;
+        }
+    }
+}
